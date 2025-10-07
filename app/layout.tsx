17bfce0e8b09ac3d35b22e19c6440f39c2886e53
Risavdeb Patra <needleweed03@gmail.com>
2025-10-07 23:42:39 +0530

    wow

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,24 +12,24 @@ const poppins = Poppins({
 });
 
 export async function generateMetadata(): Promise<Metadata> {
-  const URL = "https://farcaster-miniapp-liart.vercel.app";
+  const URL = "https://auction-house-red.vercel.app";
   return {
-    title: "Late Night on Base",
+    title: "Auction House",
     description:
-      "Daily Base ecosystem updates in 60 seconds, plus replays of top streams featuring the builders shaping the future. Hosted by Bill the Bull.",
+      "Create, bid, and trade NFTs seamlessly on Base with our Auction House.",
     other: {
       "fc:frame": JSON.stringify({
         version: "next",
-        imageUrl: "https://farcaster-miniapp-liart.vercel.app/pfp.jpg",
+        imageUrl: "https://auction-house-red.vercel.app/pfp.jpg",
         button: {
-          title: `Tune in!`,
+          title: `Bid Now!`,
           action: {
             type: "launch_frame",
-            name: "Late Night on Base",
+            name: "Auction House",
             url: URL,
             splashImageUrl:
-              "https://farcaster-miniapp-liart.vercel.app/pfp.jpg",
-            splashBackgroundColor: "#3b0404",
+              "https://auction-house-red.vercel.app/pfp.jpg",
+            splashBackgroundColor: "#000000",
           },
         },
       }),
