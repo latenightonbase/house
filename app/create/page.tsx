50da7ac9afbc163a2f1d1ce6c390872ddfc079c5
Risavdeb Patra <needleweed03@gmail.com>
2025-10-23 22:24:54 +0530

    whitelist implemented

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -3,7 +3,7 @@ import PageLayout from "@/components/UI/PageLayout";
 
 export default function CreatePage() {
     return (
-        <PageLayout className="min-h-screen">
+        <PageLayout className="">
             <h1 className="text-2xl font-bold gradient-text">Start an Auction!</h1>
             <p className="text-caption text-sm mt-4">Fill out these fields and start receiving bids in your Base coin of choice.</p>
             <CreateAuction/>
